487bcf1ac810a011d186ecbd51c40eaacb428725
Filippo Patrignani <filippo.patrignani2@studio.unibo.it>
2024-12-12 15:28:11 +0100

    Update esercizi.sh

--- esercizi.sh
+++ esercizi.sh
@@ -1,3 +1,4 @@
 cut -b 3-5,10-15 testo.txt ;
 cut -b 3-5,10-15 testo.txt | sed 's/*//g' | sed 's/ //g' | sed 's/\t//g' ;
 cut -b 3-5,10-15 testo.txt | sed 's/*//g' | sed 's/ //g' | sed 's/\t//g' | wc -c ;
+wget http://www.cs.unibo.it/~ghini/didattica/sistemioperativi/ESERCIZI/esprcond_errato.sh
